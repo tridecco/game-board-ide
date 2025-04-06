b94e12ca4bc81c00c3304b6acf6b98b56ce67726
Tony Kan <tianchengkan@gmail.com>
2025-04-06 14:45:39 -0700

    feat(app): Implement main application class and initialize UI module

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,26 @@
  * @fileoverview Index File
  * @description This is the main entry point for the JavaScript.
  */
+
+const UI = require('./ui');
+
+/**
+ * @class App - A class for indexing the application.
+ */
+class App {
+  /**
+   * @constructor
+   */
+  constructor() {
+    this.ui = new UI(this);
+  }
+}
+
+const appInstance = new App();
+
+Object.defineProperty(window, 'app', {
+  value: appInstance,
+  writable: false,
+  configurable: false,
+  enumerable: true,
+});
