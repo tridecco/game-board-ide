d55fcbaa411bdabe81b70ed31347de4990707aca
Tony Kan <tianchengkan@gmail.com>
2025-04-06 14:58:47 -0700

    refactor(index): Remove App class and instantiate UI module directly

--- src/js/index.js
+++ src/js/index.js
@@ -3,25 +3,5 @@
  * @description This is the main entry point for the JavaScript.
  */
 
+// Import UI module
 const UI = require('./ui');
-
-/**
- * @class App - A class for indexing the application.
- */
-class App {
-  /**
-   * @constructor
-   */
-  constructor() {
-    this.ui = new UI(this);
-  }
-}
-
-const appInstance = new App();
-
-Object.defineProperty(window, 'app', {
-  value: appInstance,
-  writable: false,
-  configurable: false,
-  enumerable: true,
-});
