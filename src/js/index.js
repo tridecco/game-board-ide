6fa1a408f67a329144b0c429520f0cbdfe0cf48f
Tony Kan <tianchengkan@gmail.com>
2025-04-06 19:30:50 -0700

    feat(pages): Add Pages module for managing page switching and update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,19 @@ const FileSystem = require('./fs');
 const UI = require('./ui');
 const Editor = require('./editor');
 const Console = require('./console');
+const Pages = require('./pages');
 
 // Import page scripts
 const pageScripts = require('./scripts');
+
+// Initialize the pages
+const pages = new Pages([
+  'home-container',
+  'editor-container',
+  'files-container',
+  'not-supported-container',
+]);
+pages.switchTo('home-container'); // Default to home page
 
 // Run the scripts
 pageScripts();
